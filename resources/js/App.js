da497d5e38b65ab3e2c1232ee64f5b827bdff2aa
Richard Yu <vipzhicheng@gmail.com>
2018-08-12 04:43:07 +0800

    adjust code

--- resources/js/App.js
+++ resources/js/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Admin, Resource } from 'react-admin'
 
-// import jsonServerProvider from 'ra-data-json-server'
-import dataProvider from './dataProvider'
+import jsonServerProvider from 'ra-data-json-server'
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
+
+// import dataProvider from './dataProvider'
 import { PostList } from './posts'
 
-// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} />
