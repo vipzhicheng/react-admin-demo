2d04ee1682b4ef544d16f0c7bc8afe2940331102
Richard Yu <vipzhicheng@gmail.com>
2018-08-12 16:01:06 +0800

    adjust code

--- app/Mocks/Post.js
+++ app/Mocks/Post.js
@@ -1,21 +1,29 @@
 const Mock = require('mockjs')
+const _ = require('lodash')
 
 module.exports = {
   index({ request, response, params }) {
     const total = 100
     const [field, sort] = request.input('sort')
-    const [offset, limit] = request.input('range')
+      ? JSON.parse(request.input('sort'))
+      : []
+    let [offset, perPage] = request.input('range')
+      ? JSON.parse(request.input('range'))
+      : []
     let id = sort == 'DESC' ? 100 : 1
+
     return {
-      data: [...Array(total).keys()].map(() => {
-        return {
-          id: Mock.mock(function() {
-            return sort == 'DESC' ? id-- : id++
-          }),
-          title: Mock.mock('@sentence(5)'),
-          body: Mock.mock('@paragraph')
-        }
-      }),
+      data: [...Array(total).keys()]
+        .map(() => {
+          return {
+            id: Mock.mock(function() {
+              return sort == 'DESC' ? id-- : id++
+            }),
+            title: Mock.mock('@sentence(3)'),
+            body: Mock.mock('@paragraph(2)')
+          }
+        })
+        .slice(offset, offset + perPage),
       total
     }
   }
--- resources/js/App.js
+++ resources/js/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Admin, Resource } from 'react-admin'
 
-// import jsonServerProvider from 'ra-data-json-server'
-// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
+import jsonServerProvider from 'ra-data-json-server'
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
 
-import dataProvider from './dataProvider'
+// import dataProvider from './dataProvider'
 import { PostList } from './posts'
 
 const App = () => (
